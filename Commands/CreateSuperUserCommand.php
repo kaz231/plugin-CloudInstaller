96116c071c736901ad5dcb8ad27ed03d9934274d
Kamil Zajac <kamil@piwik.pro>
2016-04-06 00:48:08 +0200

    - added command that allows to create super user - added command that allows to create website

---
+++ Commands/CreateSuperUserCommand.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Copyright (C) Piwik PRO - All rights reserved.
+ *
+ * Using this code requires that you first get a license from Piwik PRO.
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ *
+ * @link http://piwik.pro
+ */
+namespace Piwik\Plugins\ConsoleInstaller\Commands;
+
+use Piwik\Access;
+use Piwik\Plugin\ConsoleCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Piwik\Plugins\UsersManager\API as UserManagerAPI;
+
+/**
+ * Class CreateSuperUserCommand
+ * @package Piwik\Plugins\ConsoleInstaller\Commands
+ */
+class CreateSuperUserCommand extends ConsoleCommand
+{
+    const USERNAME_ARG = 'username';
+    const PASSWORD_ARG = 'password';
+    const EMAIL_ARG = 'email';
+
+    const ALIAS_OPTION = 'alias';
+    const HASHED_PASSWORD = 'hashed-password';
+
+    protected function configure()
+    {
+        $this
+            ->setName('console-installer:create-super-user')
+            ->setDescription('Allows to create super user')
+            ->addArgument(self::USERNAME_ARG, InputArgument::REQUIRED)
+            ->addArgument(self::PASSWORD_ARG, InputArgument::REQUIRED)
+            ->addArgument(self::EMAIL_ARG, InputArgument::REQUIRED)
+            ->addOption(self::ALIAS_OPTION, null, InputOption::VALUE_OPTIONAL)
+            ->addOption(
+                self::HASHED_PASSWORD,
+                null,
+                InputOption::VALUE_NONE,
+                'Use this flag if provided password was encoded with MD5'
+            );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $username = $input->getArgument(self::USERNAME_ARG);
+        $password = $input->getArgument(self::PASSWORD_ARG);
+        $email = $input->getArgument(self::EMAIL_ARG);
+
+        $alias = $input->getOption(self::ALIAS_OPTION);
+        $isPasswordHashed = $input->getOption(self::HASHED_PASSWORD) ? true : false;
+
+        Access::doAsSuperUser(function () use ($username, $password, $email, $alias, $isPasswordHashed) {
+            UserManagerAPI::getInstance()->addUser(
+                $username,
+                $password,
+                $email,
+                $alias ?: false,
+                $isPasswordHashed
+            );
+
+            UserManagerAPI::getInstance()->setSuperUserAccess($username, true);
+        });
+    }
+}
+ 
---
+++ Commands/CreateWebsiteCommand.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Copyright (C) Piwik PRO - All rights reserved.
+ *
+ * Using this code requires that you first get a license from Piwik PRO.
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ *
+ * @link http://piwik.pro
+ */
+namespace Piwik\Plugins\ConsoleInstaller\Commands;
+
+use Piwik\Access;
+use Piwik\Plugin\ConsoleCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Piwik\Plugins\SitesManager\API as SitesManagerAPI;
+
+/**
+ * Class CreateWebsiteCommand
+ * @package Piwik\Plugins\ConsoleInstaller\Commands
+ */
+class CreateWebsiteCommand extends ConsoleCommand
+{
+    const NAME_ARG = 'name';
+    const URL_ARG = 'url';
+
+    const ENABLE_ECOMMERCE_OPTION = 'enable-ecommerce';
+    const TIMEZONE_OPTION = 'timezone';
+    const CURRENCY_OPTION = 'currency';
+    const START_DATE_OPTION = 'start-date';
+
+    protected function configure()
+    {
+        $this
+            ->setName('console-installer:create-website')
+            ->addArgument(self::NAME_ARG, InputArgument::REQUIRED)
+            ->addArgument(self::URL_ARG, InputArgument::REQUIRED)
+            ->addOption(self::ENABLE_ECOMMERCE_OPTION, null, InputOption::VALUE_NONE)
+            ->addOption(self::TIMEZONE_OPTION, null, InputOption::VALUE_OPTIONAL)
+            ->addOption(self::CURRENCY_OPTION, null, InputOption::VALUE_OPTIONAL)
+            ->addOption(self::START_DATE_OPTION, null, InputOption::VALUE_OPTIONAL);
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $name = $input->getArgument(self::NAME_ARG);
+        $url = $input->getArgument(self::URL_ARG);
+
+        $eCommerce = $input->getOption(self::ENABLE_ECOMMERCE_OPTION) ? true : false;
+        $timezone = $input->getOption(self::TIMEZONE_OPTION);
+        $currency = $input->getOption(self::CURRENCY_OPTION);
+        $startDate = $input->getOption(self::START_DATE_OPTION);
+
+        Access::doAsSuperUser(function () use ($name, $url, $eCommerce, $timezone, $currency, $startDate) {
+            SitesManagerAPI::getInstance()->addSite(
+                $name,
+                $url,
+                $eCommerce,
+                null,
+                null,
+                null,
+                null,
+                null,
+                $timezone,
+                $currency,
+                null,
+                $startDate,
+                null,
+                null,
+                null,
+                null,
+                null
+            );
+        });
+    }
+}
